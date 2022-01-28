6bd19cd4b46ae2b9b692f417334ab7c3043122c1
Louis Vanslembrouck <louisvanslembrouck@gmail.com>
2022-01-28 11:52:04 +0100

    Refactored

--- Poslog_sort/Program.cs
+++ Poslog_sort/Program.cs
@@ -48,75 +48,9 @@ namespace PoslogSort
                 }    
             }
 
-            foreach(string file in Directory.GetFiles(cwd, "*.xml")){
-
-                if(File.Exists(file)) {
-
-                    try {
-
-                        string store = getStoreID(file);
-                        string fname = file.Substring(cwd.Length + 1);
-                        string seqNr = returnSeq(file);
-                        string amount = returnAmount(file);
-
-                        if (!Directory.Exists(Path.Combine(cwd, store))){
-
-                            Directory.CreateDirectory(Path.Combine(cwd, store));
-                        }    
-                        try {
-
-                            File.Copy(file, Path.Combine(cwd, store,$"{fname} - {seqNr} - {amount}"));
-                            counter++;
-                                
-                            File.Delete(file);
-                        }
-                            catch(Exception e){
-                            Console.WriteLine(e.Message);
-                        }
-                    }
-                    catch(Exception e)  {
-
-                        Console.WriteLine(e.Message, e.InnerException);
-                    }
-                } else {
-                    continue;
-                }             
-            }
-
             Console.WriteLine($"Succesfully handled {counter} files. Failed {failed}.");
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
-        }
-
-        public static string getStoreID(string file){
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load(file);
-
-            XmlNodeList storeId = doc.GetElementsByTagName("RetailStoreID");
-
-            return storeId[0].InnerXml;
-        }
-
-        public static string returnSeq(string file){
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load(file);
-
-            XmlNodeList seq = doc.GetElementsByTagName("SequenceNumber");
-
-            return seq[0].InnerXml;
-
-        }
-
-        public static string returnAmount(string file){
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load(file);
-
-            XmlNodeList amount = doc.GetElementsByTagName("Total");
-
-            return amount[0].InnerXml;
         }
 
         public static Row returnAll(string file){ 
