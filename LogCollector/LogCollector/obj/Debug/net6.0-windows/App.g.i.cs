f56e95ea3e0ff8c2b7929b976730f14c34e4022f
Louis Vanslembrouck <louisvanslembrouck@gmail.com>
2023-04-21 16:50:44 +0200

    Push latest changes.

--- LogCollector/LogCollector/obj/Debug/net6.0-windows/App.g.i.cs
+++ LogCollector/LogCollector/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace LogCollector {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LogCollector {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             LogCollector.App app = new LogCollector.App();
             app.InitializeComponent();
